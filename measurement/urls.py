868187a2899fe623582755393853a3829be53e55
alexgusar <alexgusar@mail.ru>
2024-09-03 16:21:21 +0300

    add models, serializers

--- measurement/urls.py
+++ measurement/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from measurement.views import SensorsView, SensorView
+
 urlpatterns = [
+    path('sensors/', SensorsView.as_view()),
+    path('sensors/<pk>/', SensorView.as_view()),
     # TODO: зарегистрируйте необходимые маршруты
 ]
--- measurement/views.py
+++ measurement/views.py
@@ -1,2 +1,22 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
+
+from measurement.models import Sensor
+from measurement.serializers import SensorDetailSerializer, SensorsSerializer
+
+
+class SensorsView(ListAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorsSerializer
+
+    def post(self, request):
+        sensor = request.data
+        Sensor(name=sensor['name'], description=sensor['description']).save()
+        return Response({'status': 'Датчик добавлен успешно!'})
+
+
+class SensorView(RetrieveAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
